eea7d44d6284d143eeaa12fc7a08e46b062cfce8
Arvind82chd <58764578+Arvind82chd@users.noreply.github.com>
2023-02-17 00:08:02 -0800

    setting up server using express

--- app.js
+++ app.js
@@ -5,20 +5,34 @@ const app = express()
 //set view engine to ejs:
 app.set('view engine', 'ejs')
 
-//adding endpoints
+//adding endpoints:
+//root
 app.get('/', (req, res) => {
-    const templateVars = {
-        name: 'Balwant',
-        age: 42,
-        birthDate: '22nd June'
-    }
-    res.render('user-profile', templateVars)
+    res.render('pages/index')
+    // const templateVars = {
+    //     name: 'Balwant',
+    //     age: 42,
+    //     birthDate: '22nd June'
+    // }
+    // res.render('user-profile', templateVars)
     //    res.send('Hello')
 })
 
+//about
 app.get('/about', (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n")
+    res.render('pages/about')
 })
+
+//Career
+app.get('/career', (req, res) => {
+    res.render('pages/career')
+})
+
+//Services
+app.get('/service', (req, res) => {
+    res.render('pages/service')
+})
+
 
 
 const port = 3000
