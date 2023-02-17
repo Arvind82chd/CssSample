17b6d5cf5e61fb1fb12b8dde9b5cc66cba591905
Arvind82chd <58764578+Arvind82chd@users.noreply.github.com>
2023-02-16 20:26:47 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+
+const port = 3000
+
+app.listen(port, (req, res) => {
+    console.log(`App listening on port: ${port}`)
+})
